ef70cb8d73d21fed32d6c805d31b1a6885346f8a
Audrey <audrey.dedieu@beecoming.fr>
2023-06-14 10:55:48 +0200

    diaporama 1 et 2

--- js/accueil.js
+++ js/accueil.js
@@ -1,75 +1,22 @@
-"use strict";
+// Sélectionnez tous les éléments <img> dans les diaporamas
+const images = document.querySelectorAll(".accueil__grid-item img");
 
-// Sélection des éléments HTML correspondant aux classes des curseurs
-let imgSlider = document.getElementsByClassName("imgSlider-1");
-let imgSlider2 = document.getElementsByClassName("imgSlider-2");
-let imgSlider3 = document.getElementsByClassName("imgSlider-3");
-let imgSlider4 = document.getElementsByClassName("imgSlider-4");
-let imgSlider5 = document.getElementsByClassName("imgSlider-5");
+// Initialisez un index pour suivre l'image active
+let currentIndex = 0;
 
-// Variables de suivi de l'étape courante pour chaque curseur
-let step = 0;
-let step2 = 0;
-let step3 = 0;
-let step4 = 0;
-let step5 = 0;
-
-// Fonction pour retirer la classe 'active' de tous les éléments du curseur
-function takeOffActiveImage(slider) {
-  for (let i = 0; i < slider.length; i++) {
-    slider[i].classList.remove("active");
+// Définissez une fonction pour afficher l'image suivante
+function showNextImage() {
+  // Masquez toutes les images
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove("active");
   }
+
+  // Affichez l'image suivante
+  images[currentIndex].classList.add("active");
+
+  // Incrémentez l'index pour afficher l'image suivante
+  currentIndex = (currentIndex + 1) % images.length;
 }
 
-// Fonction d'intervalle pour afficher les images du curseur
-function interval(etape, slider) {
-  console.log("Slider length:", slider.length);
-  setInterval(() => {
-    etape++;
-    if (etape >= slider.length) {
-      etape = 0;
-    }
-    console.log("Current step:", etape);
-    takeOffActiveImage(slider);
-    slider[etape].classList.add("active");
-  }, 2500);
-}
-
-// Fonction principale pour démarrer les intervalles pour chaque curseur
-function sliders() {
-  // Démarrer l'intervalle pour chaque curseur avec son étape correspondante
-  console.log("imgSlider length:", imgSlider.length);
-  console.log("imgSlider2 length:", imgSlider2.length);
-  console.log("imgSlider3 length:", imgSlider3.length);
-  console.log("imgSlider4 length:", imgSlider4.length);
-  console.log("imgSlider5 length:", imgSlider5.length);
-
-  interval(step, imgSlider);
-  interval(step2, imgSlider2);
-  interval(step3, imgSlider3);
-  interval(step4, imgSlider4);
-  interval(step5, imgSlider5);
-}
-
-// Ajout de la classe 'active' à la première image de chaque curseur
-if (imgSlider.length > 0) {
-  imgSlider[0].classList.add("active");
-}
-
-if (imgSlider2.length > 0) {
-  imgSlider2[0].classList.add("active");
-}
-
-if (imgSlider3.length > 0) {
-  imgSlider3[0].classList.add("active");
-}
-
-if (imgSlider4.length > 0) {
-  imgSlider4[0].classList.add("active");
-}
-
-if (imgSlider5.length > 0) {
-  imgSlider5[0].classList.add("active");
-}
-
-sliders();
+// Démarrez les diaporamas en appelant la fonction showNextImage toutes les 3 secondes
+setInterval(showNextImage, 3000);
