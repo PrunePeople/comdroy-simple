afc2fff5978cedc23c58c11801c64cd87429c0e3
Audrey <audrey.dedieu@beecoming.fr>
2023-06-15 07:30:43 +0200

    diapo contact

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,6 @@
+// Formulaire
 //Récuperer le formulaire
-
 const formulaire = document.getElementById("myForm");
-
 const name = document.getElementById("idName");
 const firstname = document.getElementById("idFirstName");
 const mail = document.getElementById("idMail");
@@ -53,3 +52,32 @@ function recap() {
   formulaire.submit();
   formulaire.reset();
 }
+
+// Diaporama
+// Sélectionne toutes les images dans les conteneurs de diaporama
+const images = document.querySelectorAll('.slideshow-container .image');
+let currentImageIndex = 0;
+
+// Fonction pour afficher l'image suivante
+function showNextImage() {
+  images[currentImageIndex].style.display = 'none'; // Masque l'image actuelle
+  currentImageIndex = (currentImageIndex + 1) % images.length; // Incrémente l'index de l'image actuelle en bouclant sur la longueur totale des images
+  images[currentImageIndex].style.display = 'block'; // Affiche la nouvelle image
+}
+
+// Fonction pour afficher l'image précédente
+function showPreviousImage() {
+  images[currentImageIndex].style.display = 'none'; // Masque l'image actuelle
+  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length; // Décrémente l'index de l'image actuelle en bouclant sur la longueur totale des images
+  images[currentImageIndex].style.display = 'block'; // Affiche la nouvelle image
+}
+
+// Fonction pour changer de diapositive en fonction de la direction
+function changeSlide(direction) {
+  if (direction === 1) {
+    showNextImage(); // Appelle la fonction pour afficher l'image suivante
+  } else if (direction === -1) {
+    showPreviousImage(); // Appelle la fonction pour afficher l'image précédente
+  }
+}
+setInterval(showNextImage, 3000); // Change d'image toutes les 3 secondes (3000 ms)
